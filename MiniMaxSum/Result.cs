ee3796352e1e1766f6bc29306305d8eea3987147
davutdursun <contact@davutdursun.net>
2025-04-28 13:23:19 +0300

    Add project files.

---
+++ MiniMaxSum/Result.cs
@@ -0,0 +1,29 @@
+﻿namespace Hackerrank.MiniMaxSum;
+public class Result
+{
+    public static long[] MiniMaxSum(List<long> arr)
+    {
+        long sum = arr.Sum();
+        long max = arr.Max();
+        long min = arr.Min();
+
+        // Console.Write($"{sum-max} {sum-min}");
+
+        return [sum-max, sum-min];
+    }
+
+    [Theory]
+    [InlineData(new long[] {1,3,5,7,9}, new long[] {16,24})]
+    [InlineData(new long[] { 1, 2, 3, 4, 5 }, new long[] { 10, 14 })]
+    public void MiniMaxSumTest(long[] arr, long[] expected)
+    {
+        // Arrange
+        var input = arr.ToList();
+
+        // Act
+        var result = MiniMaxSum(input);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ PlusMinus/Result.cs
@@ -0,0 +1,41 @@
+﻿namespace Hackerrank.PlusMinus;
+public class Result
+{
+
+    public static float[] PlusMinus(List<int> arr)
+    {
+        float totalCount = arr.Count;
+        float pCount = arr.Where(x => x > 0).Count();
+        float nCount = arr.Where(x => x < 0).Count();
+        float zCount = arr.Where(x => x == 0).Count();
+
+        float pRatio = pCount / totalCount;
+        float nRatio = nCount / totalCount;
+        float zRatio = zCount / totalCount;
+
+        //Console.WriteLine(pRatio.ToString("N6"));
+        //Console.WriteLine(nRatio.ToString("N6"));
+        //Console.WriteLine(zRatio.ToString("N6"));
+
+        return [
+            (float)Math.Round(pRatio, 6),
+            (float)Math.Round(nRatio, 6),
+            (float)Math.Round(zRatio, 6)
+        ];
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 1, 0, -1, -1 }, new float[] { 0.400000f, 0.400000f, 0.200000f })]
+    [InlineData(new int[] { -4, 3, -9, 0, 4, 1 }, new float[] { 0.500000f, 0.333333f, 0.166667f })]
+    public void PlusMinusTest(int[] arr, float[] expected)
+    {
+        // Arrange
+        var input = arr.ToList();
+
+        // Act
+        var result = PlusMinus(input);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
